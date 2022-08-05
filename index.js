19f4b3d0fadb426f954e74fcea3a64a8de48e373
Krzysztof Gagacki <krzysztofgagacki@gmail.com>
2022-08-05 10:15:27 +0200

    updated styling, added hand calculation, adde drawing cards basic logic and styling

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const cardColors = ['♠','♥','♦','♣'];
 const cardValues = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+let playersHand = [], dealersHand = [], deck = [];
+let playersHandValue = 0, dealersHandValue = 0;
+const playerArea = document.getElementById('playerArea');
+const dealerArea = document.getElementById('dealerArea');
+const handValue = document.getElementById('handvalue');
+let playersTurn = true;
 
 class Card {
     color;
@@ -9,13 +15,14 @@ class Card {
     constructor (color, value) {
         this.color = color;
         this.value = value;
-        if (value === 'J' || value === 'Q' || value ==='K') this.points = 10;
+        if (value === 'J') this.points = 2;
+        else if (value === 'Q') this.points = 3;
+        else if (value ==='K') this.points = 4;
         else if (value === 'A') this.points = 11
         else this.points = Number(value);
     }
 }
 
-let deck = [];
 function createDeck () {
     let count = 0;
     for (let i = 0; i < cardColors.length; i++) {
@@ -24,20 +31,56 @@ function createDeck () {
             count++;
         }
     }
-    console.log('Deck Created');
+    console.log('New Deck Created');
 }
 
 function shuffleDeck () {
-    let temp;
-    let randIndex;
+    let temp, randIndex;
     for (let i = 0; i < deck.length; i++) {
         randIndex = Math.floor(Math.random()*52);
         temp = deck[i];
         deck[i] = deck[randIndex];
         deck[randIndex] = temp;
     }
+    console.log('Deck Shuffled');
+}
+
+function playCard() {
+    const cardToPlay = deck.pop();
+    const newCardDiv = document.createElement('div');
+    newCardDiv.classList.add('card');
+    if (playersTurn) {
+        newCardDiv.style.borderColor = (cardToPlay.color === '♥' || cardToPlay.color === '♦') ? "red" : "black";
+        newCardDiv.style.color = (cardToPlay.color === '♥' || cardToPlay.color === '♦') ? "red" : "black";
+        newCardDiv.innerHTML = `${cardToPlay.value}<br>${cardToPlay.color}`;
+        console.log(`Player drew card ${cardToPlay.value}${cardToPlay.color}`);
+        playersHand.push(cardToPlay);
+        playerArea.appendChild(newCardDiv);
+    }
+    else {
+        newCardDiv.style.backgroundColor = "dodgerblue";
+        console.log(`Dealer drew a card`);
+        dealersHand.push(cardToPlay);
+        dealerArea.appendChild(newCardDiv);
+    }
+}
+
+function calcHandValue(hand) {
+    let handValue = 0;
+    hand.forEach(element => {
+        handValue += element.points;
+    });
+    return handValue;
+}
+
+function updateHandValue() {
+    handValue.innerHTML = `<h2>Hand Value: <big>${calcHandValue(playersHand)}</big></h2>`;
 }
 
 // Program Start
-createDeck ();
+createDeck();
 shuffleDeck();
+playCard();
+playersTurn = false;
+playCard();
+updateHandValue();
